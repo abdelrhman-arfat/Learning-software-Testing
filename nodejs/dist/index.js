01162c3e21f6875a125b8579c51d9fa7560905b7
boody <abdoarfat2006@gmail.com>
2025-07-19 14:14:49 +0300

    Add test with e2e Testing in with puppeteer.js

--- nodejs/dist/index.js
+++ nodejs/dist/index.js
@@ -3,24 +3,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
+exports.server = void 0;
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const path_1 = __importDefault(require("path"));
 // configuration
 require("./config/config.js");
 // routes
 const env_js_1 = require("./constants/env.js");
-const books_router_js_1 = require("./resources/books/books.router.js");
+const books_route_js_1 = require("./resources/books/books.route.js");
 // create app instance and get port
-exports.app = (0, express_1.default)();
+const app = (0, express_1.default)();
 // configure app
-exports.app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.json());
 // map the app routes
-exports.app.use("/api/books", books_router_js_1.bookRouter);
-exports.app.use((req, res) => {
+app.use("/api/books", books_route_js_1.bookRouter);
+app.use("/page", express_1.default.static(path_1.default.join(__dirname, "..", "pages")));
+app.get("/page/home", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "..", "pages", "home.html"));
+});
+app.get("/page/form", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "..", "pages", "form.html"));
+});
+app.use((req, res) => {
     res.status(404).json({ message: "404 not found", data: null });
 });
-// bind app to the PORT
-exports.app.listen(env_js_1.PORT, () => {
+const server = app.listen(env_js_1.PORT, () => {
     console.log(`app is up and running in the ${env_js_1.NODE_ENV} mode on port ${env_js_1.PORT}`);
 });
+exports.server = server;
--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 
 // configuration
 import "./config/config.js";
@@ -17,11 +18,18 @@ app.use(bodyParser.json());
 // map the app routes
 app.use("/api/books", bookRouter);
 
+app.use("/page", express.static(path.join(__dirname, "..", "pages")));
+app.get("/page/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "pages", "home.html"));
+});
+app.get("/page/form", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "pages", "form.html"));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: "404 not found", data: null });
 });
 
-// bind app to the PORT
 const server = app.listen(PORT, () => {
   console.log(`app is up and running in the ${NODE_ENV} mode on port ${PORT}`);
 });
